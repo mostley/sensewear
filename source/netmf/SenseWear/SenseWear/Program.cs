1a463b6d90174a30ed5511242d29899030412107
Sven Hecht <sven.hecht@gmail.com>
2014-04-24 22:30:12 +0200

    clean up, added netmf solution

---
+++ source/netmf/SenseWear/SenseWear/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.SPOT;
+
+namespace SenseWear
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("Amazing!");
+        }
+
+    }
+}
